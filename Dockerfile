ac15fd77897f72dba547fc7dde9907d96c246fa8
izzat <tiran.sss>
2021-04-10 10:58:22 +0500

    everything changed

--- Dockerfile
+++
@@ -1,9 +0,0 @@
-FROM python:3.8
-
-RUN mkdir -p /usr/src/app/
-WORKDIR /usr/src/app/
-
-COPY . /usr/src/app/
-RUN pip install --no-cache-dir -r requirements.txt
-
-CMD ["python","main.py"]
---
+++ test.py
@@ -0,0 +1,64 @@
+from flask import Flask
+from flask import request
+import requests
+
+app = Flask(__name__)
+URL = 'https://api.telegram.org/bot1781862582:AAG2GInZDiz-sK_-q56M_u5b-lACCejoFpg/'
+
+
+@app.route('/', methods=['POST', 'GET'])
+def index():
+    if request.method == 'POST':
+        r = request.get_json()
+        chat_id = r['message']['chat']['id']
+        text = r['message']['text']
+        data = {
+            'login': text
+        }
+        r = requests.post('https://e-tashpmi.uz/bot/student_statistic/', data=data)
+        if r.status_code == 201:
+            send_document(chat_id, text)
+        elif r.status_code == 404:
+            send_message(chat_id, text='Такого ID  не существует ')
+        elif r.status_code == 200:
+            text = '''{}
+    Курс {}
+    ID  {}
+    Оплаченная сумма {}
+    Задолжность за первый семестер {}
+    Задолжность за второй семестер {}
+    Задолжность за учебный год {}
+    ------------------------------
+    Для полного ознакомления с предоставленными данными 
+    можете обратиться в Маркетинг с 10:00 до 18:00 
+    От понедельника до пятницы'''.format(r.json()['name'], r.json()['level'], r.json()['id_number'],
+                                         r.json()['account']['overall_payment'],
+                                         r.json()['account']['debt_for_first_semester'],
+                                         r.json()['account']['debt_for_second_semester'],
+                                         r.json()['account']['overall_debt'], )
+
+            send_message(chat_id, text)
+    return '<h1> Bot is running </h1>'
+
+
+def send_message(chat_id, text):
+    url = URL + 'sendMessage'
+    answer = {'chat_id': chat_id, 'text': text}
+    r = requests.post(url=url, json=answer)
+    return r.json()
+
+
+def send_document(chat_id, text):
+    url = URL + 'sendDocument'
+    PATH = '/home/izzat/tashpmi/media'
+    with open(PATH + text + '.docx', 'rb') as doc:
+        files = {"document": doc}
+        title = '555.docx'
+        r = requests.post(url=url, data={"chat_id": chat_id, "caption": title}, files=files)
+        if r.status_code != 200:
+            raise Exception("send error")
+    return r.json()
+
+
+if __name__ == '__main__':
+    app.run()
